19a3a4ad5e9643e867e3210198f0eff36b0aa6c0
amaher <amaher112@su.edu>
2023-12-13 16:54:01 -0500

    added event listener

--- Develop/script.js
+++ Develop/script.js
@@ -8,16 +8,38 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  var saveBtn = $('.saveBtn');
+
+  $(saveBtn).on("click", function() {
+  //     var parentElement = this.parentNode;
+  //     var parentId = parentElement.id;
+  // console.log(parentId)
+
+//     var key = saveBtn.parent().attr("id");
+//  console.log();    
+var siblingElement = this.previousElementSibling;
+
+console.log(siblingElement.val());
+// var input = $("textarea").val();
+//     console.log(input);
+
+//     localStorage.setItem(parentId, input);
+
+
+    
+    // Key equals the div id of the parent
+    
+    // Value equals the textarea.value that's the sibling of the save button
+  })
+  
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
-
   var currentHour = dayjs().hour();
-
   var rowEl = $("textarea");
 
   for (var i = 0; i < rowEl.length; i++) {
